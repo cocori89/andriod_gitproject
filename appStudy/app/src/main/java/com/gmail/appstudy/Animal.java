35d5bfac75e6c3cfcba39973f168b3328e871cf6
coco <ckddls89@gmail.com>
2017-11-09 12:16:24 +0900

    Animal 클래스 만듬

---
+++ appStudy/app/src/main/java/com/gmail/appstudy/Animal.java
@@ -0,0 +1,15 @@
+package com.gmail.appstudy;
+
+/**
+ * Created by USER on 2017-11-09.
+ */
+/*동물 클래스~*/
+public class Animal {
+    public int leg;// 도물의 다리 객수를 정의 하는 변수
+    public String color; // 동물의 색상을 정의하는 변수
+    public void eating(){
+        //맛있게 무언가를 먹고 있는 중...
+
+
+    }
+}
--- appStudy/app/src/main/java/com/gmail/appstudy/MainActivity.java
+++ appStudy/app/src/main/java/com/gmail/appstudy/MainActivity.java
@@ -7,7 +7,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*부모에 있는 메소드를 그대로 다시 가져 온다*/
         super.onCreate(savedInstanceState);
+        /*거기에 추각적인 내용이 추가되는 것이다. */
         setContentView(R.layout.activity_main);
     }
 }
