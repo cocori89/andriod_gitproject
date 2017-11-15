81cce29824aee0895aff26fb2e980431bc414031
coco <ckddls89@gmail.com>
2017-11-15 18:34:33 +0900

    아이디 받아 오는 것 완성 그리고 지금 점수 판명 이상함 , 그리고 조커의 경우 시 넣어 줘야 하고 점수 판명 할떄 똑같아도 통과 가능 하게 해야함

--- StreamsBoardGame/app/src/main/java/com/gmail/streamsboardgame/NormalActivity.java
+++ StreamsBoardGame/app/src/main/java/com/gmail/streamsboardgame/NormalActivity.java
@@ -9,20 +9,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class NormalActivity extends AppCompatActivity {
+    /*변수 시작*/
     int numTile[];// 섞은 타일 받아 오는 변수
-    int tileIntex=0;// 출력할 인뎃스 값
-    String numberString="";// 클릭하면 할때 마다 담을 변수
+    int tileIntex = 0;// 출력할 인뎃스 값
+    String numberString = "";// 클릭하면 할때 마다 담을 변수
 
     /*텍스트 뷰 변수*/
-    TextView textView0,textView1,textView2,textView3,textView4,textView5,textView6,textView7,
-            textView8,textView9,textView10,textView11,textView12,textView13,textView14,textView15,
-            textView16,textView17,textView18,textView19;
+    TextView[] textViews;//텍스트 뷰의 아이디 값을 받아올 배열
     ArrayList<TextView> textViewArrayList;//뷰 변수들이 들어갈 변수
 
-    /*공사중 텍스트 뷰 변수 배열*/
-    TextView[] textViews;
+    /*시트의 정보 변수들*/
+    Button[] buttons;// 버튼의 정보를 받아온다.
+    HashMap<Button, Integer> buttonIntegerHashMap;// 버튼의 ID 키로 해서 점수를 위한 배열의 위치를 받아내기 위한 용도
+    Integer[] resultNumber;// 최종 완료된 시트 숫자 배열 점수를 판단하기 위한 시트의 번호가 들어가 있음
+
+    /*변수 끝 --------------------------------------*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,112 +36,79 @@ public class NormalActivity extends AppCompatActivity {
         setTitle("일반 모든 스트림스");
 
         /*게임이 시작되면서 타일 세팅과 게임 초기화*/
-        NumberTile numberTile =  new NumberTile();// 타일 섞을 개체 만들기
+        NumberTile numberTile = new NumberTile();// 타일 섞을 개체 만들기
         numberTile.mixTile();//타일 섞기
-        numTile=new int[40];
+        numTile = new int[40];
         numTile = numberTile.getNumberTile();// 섞은 타일 얻기
 
-
         /*시작 하자 마자 뷰 들을 리스트에 넣어주기 */
+        textViewArrayList = new ArrayList<TextView>();
         textViews = new TextView[20];
-        for(int i = 0 ; i<20 ; i++){
-            
-
+        for (int i = 0; i < 20; i++) {
+            textViews[i] = (TextView) findViewById(getResources().getIdentifier("textView" + i, "id", "com.gmail.streamsboardgame"));
+            textViewArrayList.add(textViews[i]);
         }
-        textView0=findViewById(R.id.textView0);
-        textView1=findViewById(R.id.textView1);
-        textView2=findViewById(R.id.textView2);
-        textView3=findViewById(R.id.textView3);
-        textView4=findViewById(R.id.textView4);
-        textView5=findViewById(R.id.textView5);
-        textView6=findViewById(R.id.textView6);
-        textView7=findViewById(R.id.textView7);
-        textView8=findViewById(R.id.textView8);
-        textView9=findViewById(R.id.textView9);
-        textView10=findViewById(R.id.textView10);
-        textView11=findViewById(R.id.textView11);
-        textView12=findViewById(R.id.textView12);
-        textView13=findViewById(R.id.textView13);
-        textView14=findViewById(R.id.textView14);
-        textView15=findViewById(R.id.textView15);
-        textView16=findViewById(R.id.textView16);
-        textView17=findViewById(R.id.textView17);
-        textView18=findViewById(R.id.textView18);
-        textView19=findViewById(R.id.textView19);
 
-        /*리스크에 넣기*/
-        textViewArrayList=new ArrayList<TextView>();
-        textViewArrayList.add(textView0);
-        textViewArrayList.add(textView1);
-        textViewArrayList.add(textView2);
-        textViewArrayList.add(textView3);
-        textViewArrayList.add(textView4);
-        textViewArrayList.add(textView5);
-        textViewArrayList.add(textView6);
-        textViewArrayList.add(textView7);
-        textViewArrayList.add(textView8);
-        textViewArrayList.add(textView9);
-        textViewArrayList.add(textView10);
-        textViewArrayList.add(textView11);
-        textViewArrayList.add(textView12);
-        textViewArrayList.add(textView13);
-        textViewArrayList.add(textView14);
-        textViewArrayList.add(textView15);
-        textViewArrayList.add(textView16);
-        textViewArrayList.add(textView17);
-        textViewArrayList.add(textView18);
-        textViewArrayList.add(textView19);
+        /*시작하자마자 시트버튼들의 정보를 정리 해준다.*/
+        buttons = new Button[20];
+        buttonIntegerHashMap = new HashMap<Button, Integer>();
+        resultNumber = new Integer[20];
+        for (int i = 0; i < 20; i++) {
+            buttons[i] = (Button) findViewById(getResources().getIdentifier("button" + i, "id", "com.gmail.streamsboardgame"));
+            buttonIntegerHashMap.put(buttons[i], i);
+        }
 
-        /*시트 아이디값 얻기*/
-        int getId = getResources().getIdentifier("button"+2,"id","com.gmail.streamsboardgame");
-        String coco =String.valueOf(getId);
-        Toast.makeText(getApplicationContext(),""+coco,Toast.LENGTH_SHORT).show();
-
-    }
+    }//onCreat() end-----------------------------------------------------
 
     //버튼 누르면 타일 숫자 출력
-    void setNumberTile(View v){
+    void setNumberTile(View v) {
 
         //20번 뽑으면 끝
-        if(tileIntex==20){
+        if (tileIntex >= 20) {
+            Toast.makeText(getApplicationContext(), "숫자를 시트에 넣어주세요", Toast.LENGTH_SHORT).show();
             return;
         }
         //시트에 숫자를 넣지 않으면 더이상 진행 하면 안됨
-        if(!(numberString.equals(""))){
-            Toast.makeText(getApplicationContext(),"숫자를 시트에 넣어주세요",Toast.LENGTH_SHORT).show();
+        if (!(numberString.equals(""))) {
+            Toast.makeText(getApplicationContext(), "숫자를 시트에 넣어주세요", Toast.LENGTH_SHORT).show();
             return;
         }
         TextView temp = textViewArrayList.get(tileIntex); //  숫자를 가져 온다.
-        if(numTile[tileIntex]==99){
+        if (numTile[tileIntex] == 99) {
             numberString = "조커☆";
             temp.setText("조커☆");// 표에 넣어준다.
-        }else {
+        } else {
             numberString = String.valueOf(numTile[tileIntex]); // 시트에 넣어줄 문자 저장한다.
             temp.setText(String.valueOf(numTile[tileIntex]));// 표에 넣어준다.
         }
         tileIntex++;
-    }
-
-
-
+    }//setNumberTile() end------------------------------------------------
 
     /*시트 버튼을 누르면 들어갈수 있도록 하기 */
-    void setNumSeat(View v){
+    void setNumSeat(View v) {
         Button tempButton = (Button) findViewById(v.getId());
         /*시트에 연속으로 들어 갈수 없게 하기*/
-        if(numberString.equals("")){
-            Toast.makeText(getApplicationContext(),"타일을 뽑아주세요",Toast.LENGTH_SHORT).show();
+        if (numberString.equals("")) {
+            Toast.makeText(getApplicationContext(), "타일을 뽑아주세요", Toast.LENGTH_SHORT).show();
             return;
         }
         /*빈칸에만 숫자 들어 갈수 있도록 하기*/
-        if(tempButton.getText().toString().equals("빈칸")){
-            tempButton.setText(numberString);
-            Toast.makeText(getApplicationContext(),"시트에"+numberString+"들어갔습니다. ",Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(getApplicationContext(),"이미 다른 숫자가 들어가 있습니다.",Toast.LENGTH_SHORT).show();
+        if (tempButton.getText().toString().equals("빈칸")) {
+            tempButton.setText(numberString);//시트에 숫자자가 들어감
+            //그거와 함께 점수를 반별한 배열에 들어가야한다.
+            Integer indexNum = buttonIntegerHashMap.get(tempButton);// 배열의 index값을 뽑아낸다
+            resultNumber[indexNum] = Integer.parseInt(numberString);
+
+            Toast.makeText(getApplicationContext(), "시트에" + numberString + "들어갔습니다. ", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "이미 다른 숫자가 들어가 있습니다.", Toast.LENGTH_SHORT).show();
         }
-        numberString="";
+        numberString = "";
+    }//setNumSeat() end -------------------------------------------------------
+
+    void finalScore(View v) {
+        Score score = new Score();
+        int finalScoreText = score.getNormalScore(resultNumber);
+        Toast.makeText(getApplicationContext(),""+finalScoreText,Toast.LENGTH_LONG).show();
     }
-
-
 }
--- StreamsBoardGame/app/src/main/java/com/gmail/streamsboardgame/Score.java
+++ StreamsBoardGame/app/src/main/java/com/gmail/streamsboardgame/Score.java
@@ -1,45 +1,54 @@
 package com.gmail.streamsboardgame;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 //점수를 판단하는 계산기
 public class Score {
-    ArrayList<Integer> normalScoreList;
+    HashMap<Integer,Integer> normalScoreMap;
 
     public Score() {// 생성자
         //객체 생성과 동시에 점수 기분표를 만들어준다.
         //일반
-        normalScoreList = new ArrayList<Integer>();
-        normalScoreList.add(1,0);
-        normalScoreList.add(2,1);
-        normalScoreList.add(3,3);
-        normalScoreList.add(4,5);
-        normalScoreList.add(5,7);
-        normalScoreList.add(6,9);
-        normalScoreList.add(7,11);
-        normalScoreList.add(8,15);
-        normalScoreList.add(9,20);
-        normalScoreList.add(10,25);
-        normalScoreList.add(11,30);
-        normalScoreList.add(12,35);
-        normalScoreList.add(13,40);
-        normalScoreList.add(14,50);
-        normalScoreList.add(15,60);
-        normalScoreList.add(16,70);
-        normalScoreList.add(17,85);
-        normalScoreList.add(18,100);
-        normalScoreList.add(19,150);
-        normalScoreList.add(20,300);
+        normalScoreMap = new HashMap<Integer,Integer>();
+        normalScoreMap.put(0,0);
+        normalScoreMap.put(1,0);
+        normalScoreMap.put(2,1);
+        normalScoreMap.put(3,3);
+        normalScoreMap.put(4,5);
+        normalScoreMap.put(5,7);
+        normalScoreMap.put(6,9);
+        normalScoreMap.put(7,11);
+        normalScoreMap.put(8,15);
+        normalScoreMap.put(9,20);
+        normalScoreMap.put(10,25);
+        normalScoreMap.put(11,30);
+        normalScoreMap.put(12,35);
+        normalScoreMap.put(13,40);
+        normalScoreMap.put(14,50);
+        normalScoreMap.put(15,60);
+        normalScoreMap.put(16,70);
+        normalScoreMap.put(17,85);
+        normalScoreMap.put(18,100);
+        normalScoreMap.put(19,150);
+        normalScoreMap.put(20,300);
         //상급자 : 상급자라면 나중에 파라미터 값을 방아서  4=3 , 12 = 20 , 17= 50  번을 바꿔 줄것
     }
 
 
     /*일반 모드 점수 결과*/
-    int getNormalScore() {
-        int resultScore = 0;//점수 결과
-
-
+    int getNormalScore(Integer[] resultNumber) {
+        Integer resultScore = 0;//점수 결과
+        int SNum=0;
+        for(int i = 1 ;  i<resultNumber.length;i++){
+            if(resultNumber[i-1]<resultNumber[i]){
+                SNum++;
+            }else {
+                resultScore =resultScore+ normalScoreMap.get(SNum);
+                SNum=0;
+            }
+        }
         return resultScore;
     }
 
