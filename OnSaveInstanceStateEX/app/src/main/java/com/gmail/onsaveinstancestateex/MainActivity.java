09f4e26865ad0feb63fefb96ca4e89d46f9ec73b
coco <ckddls89@gmail.com>
2017-11-20 11:40:32 +0900

    상태 저장 완료 test

--- OnSaveInstanceStateEX/app/src/main/java/com/gmail/onsaveinstancestateex/MainActivity.java
+++ OnSaveInstanceStateEX/app/src/main/java/com/gmail/onsaveinstancestateex/MainActivity.java
@@ -2,12 +2,26 @@ package com.gmail.onsaveinstancestateex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    // 숫자 입력
+    String[] testText = {"일","이","삼"};
+
+    // 텍스트 뷰
+    // 텍스뷰 리스트
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+
+
+    }
+
+    public void testClick(View v){
+
     }
 }
