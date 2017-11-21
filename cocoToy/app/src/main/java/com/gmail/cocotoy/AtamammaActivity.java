0894d2bddb1352e61ec225591b8495bb39668df0
coco <ckddls89@gmail.com>
2017-11-21 11:38:38 +0900

    완료 아따맘마 완료

--- cocoToy/app/src/main/java/com/gmail/cocotoy/AtamammaActivity.java
+++ cocoToy/app/src/main/java/com/gmail/cocotoy/AtamammaActivity.java
@@ -3,15 +3,31 @@ package com.gmail.cocotoy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AtamammaActivity extends AppCompatActivity {
+
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atamamma);
+
+
+
+
+
     }
 
+
+    public void onClickShake(View v){
+        img = findViewById(R.id.imageView);
+        img.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake));
+    }
 
 
 
--- cocoToy/app/src/main/java/com/gmail/cocotoy/MainActivity.java
+++ cocoToy/app/src/main/java/com/gmail/cocotoy/MainActivity.java
@@ -5,7 +5,6 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer bgm;// 아따 맘마 배경음악
@@ -15,11 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /*아따맘마 배경음악*/
-        bgm = MediaPlayer.create(this,R.raw.Atamamma_hello);// (여기 이 엑티비티에서 실행하며, R.raw.Atamamma_hello에 있는 자원을 만든다.)
+        bgm = MediaPlayer.create(this,R.raw.atamamma_hello);// (여기 이 엑티비티에서 실행하며, R.raw.Atamamma_hello에 있는 자원을 만든다.)
         bgm.setLooping(true);// 연속적으로 계속 해서 재생한다.
         bgm.start();// 음악지 재생됨
-
-
     }
 
     //아따 맘마로 이동
